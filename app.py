0e20bb426fccdbe1b4316ea8aa96568d4f4ea400
Uleko Samuel <samuelblessing142008@gmail.com>
2026-01-02 00:25:54 +0100

    Add update[PUT] and delete[DELETE] for better functionality

--- app.py
+++ app.py
@@ -36,6 +36,33 @@ def create_task():
 def get_tasks():
     return jsonify(tasks), 200
 
+@app.route("/tasks/<int:task_id>", methods=["PUT"])
+def update_task(task_id):
+
+    data = request.get_json()
+
+    for task in tasks:
+        if task["id"] == task_id:
+
+            if "title" in data:
+                task["title"] = data["title"]
+
+            if "completed" in data:
+                task["completed"] = data["completed"]
+            return jsonify(task), 200
+
+    return jsonify({"error":"Task not found"}), 404
+
+@app.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+
+    global tasks
+
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return jsonify({"message":"Task deleted"}), 200
+
 
 if __name__ == "__main__":
      app.run(debug=True, )
